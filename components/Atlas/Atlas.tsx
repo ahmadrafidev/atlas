e4df03f444f51c6a424f074cefec293581c1fe36
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-11-29 22:27:51 +0700

    refine the Ui when issue is shown

--- components/Atlas/Atlas.tsx
+++ components/Atlas/Atlas.tsx
@@ -12,6 +12,7 @@ export default function Atlas() {
   const [issues, setIssues] = useState<Issue[]>([]);
   const [isScanning, setIsScanning] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [reportDate, setReportDate] = useState<string | null>(null);
 
   const handleScan = async () => {
     setError(null);
@@ -22,9 +23,14 @@ export default function Atlas() {
     }
 
     setIsScanning(true);
+
     try {
       const result: Issue[] = await scanWebsite(url);
       setIssues(result);
+      setReportDate(new Date().toLocaleString('en-US', {
+        dateStyle: 'medium',
+        timeStyle: 'long',
+      }));
     } catch (error) {
       console.error('Scanning error:', error);
       setError('An error occurred while scanning the website.');
@@ -34,20 +40,27 @@ export default function Atlas() {
 
   return (
     <div className="flex items-center justify-center">
-      <main className="flex flex-col items-center justify-center px-4 text-center max-w-2xl w-full">
-        <div className="pt-14">
-          <h1 className="text-4xl font-bold text-black mb-2 tracking-normal">
-            Website Accessibility Analysis,{' '}
-            <span className="text-5xl tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-blue-500 to-violet-500">
-              Simplified
-            </span>
-          </h1>
-          <p className="text-gray-600 text-lg mb-6">
-            Effortlessly analyze your websites accessibility.
-          </p>
-        </div>
+      <main className="flex flex-col items-center justify-center px-4 text-center max-w-3xl w-full">
+        {!issues.length && (
+          <div className="pt-14">
+            <h1 className="text-4xl font-bold text-black mb-2 tracking-normal">
+              Website Accessibility Analysis,{' '}
+              <span className="text-5xl tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-blue-500 to-violet-500">
+                Simplified
+              </span>
+            </h1>
+            <p className="text-gray-600 text-lg mb-6">
+              Effortlessly analyze your websites accessibility.
+            </p>
+          </div>
+        )}
 
         <div className="flex flex-col w-full space-y-2">
+          {reportDate && (
+            <p className="text-gray-800 text-xl text-left tracking-tight font-medium">
+              Report from {reportDate}
+            </p>
+          )}
           <div className="flex w-full space-x-2">
             <Input
               type="url"
